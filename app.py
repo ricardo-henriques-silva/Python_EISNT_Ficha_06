d4c2dd8e768fc1d1555ac3f1783fa0ffcaa133dc
ricardo-henriques-silva <134967737+ricardo-henriques-silva@users.noreply.github.com>
2023-07-12 23:13:12 +0100

    Versão 2 com problemas de redireccionamento resolvidos

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from forms import AvancarForm, RespostaForm, MensagemForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'segredo'
 
-respostas_correctas = ['lisboa', 'laranja', 'resposta3']
+respostas_correctas = 'lisboa'
 mensagens = []
 
 @app.route("/", methods=['GET', 'POST'])
@@ -16,17 +16,16 @@ def home(): # alterar código seguinte
     return render_template('home.html', form=form_avancar)
 
 @app.route("/desafios", methods=['GET', 'POST'])
-def verificar_respostas():
-    respostas_form=[] # inicialização da lista com as respostas do utilizador
-    for i in range(0,2): 
-        respostas_form.append = RespostaForm().lower() # Obter as respostas enviadas pelo formulário
-    
-    # Verificar se as respostas estão corretas
-    if respostas_form == respostas_correctas:
-        return redirect('/final')
-    else:
-        flash('Resposta incorreta. Tenta novamente.', 'error')
-    return render_template('final.html', form=respostas_form)
+def desafios():
+    #respostas_form=[] # inicialização da lista com as respostas do utilizador
+    respostas_form = RespostaForm() # Obter as respostas enviadas pelo formulário
+    if respostas_form.validate_on_submit():
+        # Verificar se as respostas estão corretas
+        if respostas_form.resposta.data == respostas_correctas:
+            return redirect(url_for('final'))
+        else:
+            flash('Resposta incorreta. Tenta novamente.', 'error')
+    return render_template('desafios.html', form=respostas_form)
 
 @app.route("/final", methods=['GET', 'POST'])
 def mensagem(): # alterar código seguinte
